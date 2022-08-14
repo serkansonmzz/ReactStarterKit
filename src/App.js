f13c4d26f1d2e06bc3c00c25302eea1b047fcbea
Serkan Sönmez <serkansonmzz@gmail.com>
2022-08-14 16:27:57 +0300

    ilk React Proje.kodları

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+     TEST 11211
+     <img src='/logo192.png'></img>
+     <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
+
+
+
